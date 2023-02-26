d644b2b3bac2816d6139a7b8aae58515c5ad3a8f
Nikita Gureev <gureev6856@gmail.com>
2023-02-26 13:07:51 +0300

    new: encrypt/decrypt via 3des (full lab1)

--- app/src/main/java/com/example/fclient/MainActivity.java
+++ app/src/main/java/com/example/fclient/MainActivity.java
@@ -2,20 +2,26 @@ package com.example.fclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.fclient.databinding.ActivityMainBinding;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'fclient' library on application startup.
     static {
         System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
     }
 
     private ActivityMainBinding binding;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +29,27 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        int res = initRng();
+        byte[] key = randomBytes(16);
+
+        String strKey = "1234567890123456";
+        String strData = "Hello, World ;)";
+        key = strKey.getBytes();
+        byte[] data = strData.getBytes(StandardCharsets.UTF_8);
+
+        byte[] encData = encrypt(key, data);
+        byte[] decData = decrypt(key, encData);
+
         // Example of a call to a native method
         TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        tv.setText(new String(data, StandardCharsets.UTF_8));
+//        tv.setText(Arrays.toString(data));
+        tv.append("\n");
+//        tv.setText(new String(data, StandardCharsets.UTF_8));
+        tv.append(Arrays.toString(encData));
+        tv.append("\n");
+        tv.append(new String(decData, StandardCharsets.UTF_8));
+//        tv.append(Arrays.toString(decData));
     }
 
     /**
@@ -33,4 +57,8 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
