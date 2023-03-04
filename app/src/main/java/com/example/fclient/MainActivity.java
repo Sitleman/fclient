00a0bf076590116904325d1137e259a9c34d893c
Nikita Gureev <gureev6856@gmail.com>
2023-03-04 20:24:46 +0300

    new: pinpadActivity (full part 2)

--- app/src/main/java/com/example/fclient/MainActivity.java
+++ app/src/main/java/com/example/fclient/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.fclient.databinding.ActivityMainBinding;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityResultLauncher activityResultLauncher;
 
     // Used to load the 'fclient' library on application startup.
     static {
@@ -29,27 +42,77 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        int res = initRng();
-        byte[] key = randomBytes(16);
 
-        String strKey = "1234567890123456";
-        String strData = "Hello, World ;)";
-        key = strKey.getBytes();
-        byte[] data = strData.getBytes(StandardCharsets.UTF_8);
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback() {
+                    @Override
+                    public void onActivityResult(Object res) {
+                        ActivityResult result = (ActivityResult)res;
+//                        ActivityResult result;
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
-        byte[] encData = encrypt(key, data);
-        byte[] decData = decrypt(key, encData);
-
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(new String(data, StandardCharsets.UTF_8));
-//        tv.setText(Arrays.toString(data));
-        tv.append("\n");
+        // Part 1. Test encrypt/decrypt
+//        int res = initRng();
+//        byte[] key = randomBytes(16);
+//
+//        String strKey = "1234567890123456";
+//        String strData = "Hello, World ;)";
+//        key = strKey.getBytes();
+//        byte[] data = strData.getBytes(StandardCharsets.UTF_8);
+//
+//        byte[] encData = encrypt(key, data);
+//        byte[] decData = decrypt(key, encData);
+//
+//        // Example of a call to a native method
+//        TextView tv = binding.sampleText;
 //        tv.setText(new String(data, StandardCharsets.UTF_8));
-        tv.append(Arrays.toString(encData));
-        tv.append("\n");
-        tv.append(new String(decData, StandardCharsets.UTF_8));
-//        tv.append(Arrays.toString(decData));
+////        tv.setText(Arrays.toString(data));
+//        tv.append("\n");
+////        tv.setText(new String(data, StandardCharsets.UTF_8));
+//        tv.append(Arrays.toString(encData));
+//        tv.append("\n");
+//        tv.append(new String(decData, StandardCharsets.UTF_8));
+////        tv.append(Arrays.toString(decData));
+
+
+    }
+
+//    public void onButtonClick(View v)
+//    {
+//        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+//        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+//        byte[] dec = decrypt(key, enc);
+//        String s = new String(Hex.encodeHex(dec)).toUpperCase();
+//        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+//    }
+
+    public void onButtonClick(View v)
+    {
+        Intent it = new Intent(this, PinpadActivity.class);
+//        startActivity(it);
+        activityResultLauncher.launch(it);
+    }
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
     }
 
     /**
